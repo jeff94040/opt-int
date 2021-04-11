69d7a22ee42ae7b89486b1463a8bad4d68ee3631
Jeff Schmidt <jeff94040@gmail.com>
2021-04-11 14:04:19 -0700

    completed option 5

--- routes/routes.js
+++ routes/routes.js
@@ -87,8 +87,8 @@ function generateFileMetrics(req){
   return fileMetrics;
 }
 
-// Load the homepage
-router.get('/', (req, res) => {
+// Render homepage
+router.get('/*', (req, res) => {
   
   const fileMetrics = generateFileMetrics(req);
 
@@ -96,10 +96,9 @@ router.get('/', (req, res) => {
 
 });
 
-// Handle post then load the homepage
-router.post('/', async (req, res) => {
-  // File upload
-  // github.com/richardgirges/express-fileupload/tree/master/example#basic-file-upload    
+// Upload file then render homepage
+// github.com/richardgirges/express-fileupload/tree/master/example#basic-file-upload
+router.post('/upload', async (req, res) => {
   if (req.files && Object.keys(req.files).length > 0){
     for (const key in req.files){
       const file = req.files[key];
@@ -110,26 +109,51 @@ router.post('/', async (req, res) => {
       }
     }
   }
-  else{ // Execute program
-    // Main menu option #2, klrint.exe (pass one)
-    if(req.body.menu == 1){
-      //const commands = ['','','','','','','','','s','1','4',req.body.xvf[0],req.body.xvf[1],req.body.xvf[2],req.body.xvf[3],'','',''];
-      const commands = ['baseline','testit','testit.op1','y','testit.stg','y','1','testit.xvf','s','1','4',req.body.xvf[0],req.body.xvf[1],req.body.xvf[2],req.body.xvf[3],'r','testit.wav','y'];
-
-      const ws = fs.createWriteStream('C:/OptInt/terminal-input.txt');
-      commands.forEach( (value) => {
-        ws.write(`${value}\r\n`);
-      });
-      ws.end();
-
-      // Event logger
-      var log = new nw.EventLogger('OptInt');
-      log.info('OptInt information log message.', 1);
-
-    }
-  }  
   // Wait 2 seconds for fs changes
   await sleep(2000);
+
+  const fileMetrics = generateFileMetrics(req);
+
+  res.render('index', fileMetrics);
+  res.redi
+});
+
+// Run a process then render homepage
+router.post('/run', async (req, res) => {
+
+  // Main menu option #2, klrint.exe (pass one)
+  if(req.body.menu == 1){
+    
+    const commands = ['baseline','testit','testit.op1','y','testit.stg','y','1','testit.xvf','s','1','4',req.body.xvf[0],req.body.xvf[1],req.body.xvf[2],req.body.xvf[3],'r','testit.wav','y'];
+
+    const ws = fs.createWriteStream('C:/OptInt/terminal-input.txt');
+    commands.forEach( (value) => {
+      ws.write(`${value}\r\n`);
+    });
+    ws.end();
+
+    // Event logger
+    var log = new nw.EventLogger('OptInt');
+    log.info('OptInt information log message.', 1);
+
+  }
+  else if(req.body.menu == 3){
+
+    const commands = ['baseline', 'testit', 'testit.op3', 'y', 'testit.shp', 'y', '1', 'testit.xvf', 's', '1', '13', req.body.xvf[0], req.body.xvf[1], req.body.xvf[2], req.body.xvf[3], req.body.xvf[4], req.body.xvf[5], req.body.xvf[6], req.body.xvf[7], req.body.xvf[8], req.body.xvf[9], req.body.xvf[10], req.body.xvf[11], req.body.xvf[12], 'r', '1', 's', '22', '42', req.body.xvf[13], req.body.xvf[14], req.body.xvf[15], req.body.xvf[16], req.body.xvf[17], req.body.xvf[18], req.body.xvf[19], req.body.xvf[20], req.body.xvf[21], req.body.xvf[22], req.body.xvf[23], req.body.xvf[24], req.body.xvf[25], req.body.xvf[26], req.body.xvf[27], req.body.xvf[28], req.body.xvf[29], req.body.xvf[30], req.body.xvf[31], req.body.xvf[32], req.body.xvf[33], 'r', '1', 'testit.vvf', 's', '1', '50', req.body.vvf[0], req.body.vvf[1], req.body.vvf[2], req.body.vvf[3], req.body.vvf[4], req.body.vvf[5], req.body.vvf[6], req.body.vvf[7], req.body.vvf[8], req.body.vvf[9], req.body.vvf[10], req.body.vvf[11], req.body.vvf[12], req.body.vvf[13], req.body.vvf[14], req.body.vvf[15], req.body.vvf[16], req.body.vvf[17], req.body.vvf[18], req.body.vvf[19], req.body.vvf[20], req.body.vvf[21], req.body.vvf[22], req.body.vvf[23], req.body.vvf[24], req.body.vvf[25], req.body.vvf[26], req.body.vvf[27], req.body.vvf[28], req.body.vvf[29], req.body.vvf[30], req.body.vvf[31], req.body.vvf[32], req.body.vvf[33], req.body.vvf[34], req.body.vvf[35], req.body.vvf[36], req.body.vvf[37], req.body.vvf[38], req.body.vvf[39], req.body.vvf[40], req.body.vvf[41], req.body.vvf[42], req.body.vvf[43], req.body.vvf[44], req.body.vvf[45], req.body.vvf[46], req.body.vvf[47], req.body.vvf[48], req.body.vvf[49], req.body.vvf[50], 'r', 'testit.op4', 'y', 'testit.pl1', 'y', 'testit.pl2', 'y', 'testit.pl3', 'y', 'testit.pl4', 'y', 'testit.pl5', 'y'];
+
+    const ws = fs.createWriteStream('C:/OptInt/terminal-input.txt');
+    commands.forEach( (value) => {
+      ws.write(`${value}\r\n`);
+    });
+    ws.end();
+
+    // Event logger
+    var log = new nw.EventLogger('OptInt');
+    log.info('OptInt information log message.', 2);
+
+  }
+
+  await sleep(4000);
 
   const fileMetrics = generateFileMetrics(req);
 

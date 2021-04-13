2638588a03b3e58d07a6ad0ff92c0d3fe582760f
Jeff Schmidt <jeff94040@gmail.com>
2021-04-12 21:16:59 -0700

    option 2 work

--- public/index.js
+++ public/index.js
@@ -9,9 +9,7 @@ const buttons = document.querySelectorAll("button");
 
 buttons.forEach( (button) => {
   button.addEventListener('click', () => {
-    buttons.forEach( (button) => {
-      button.disabled = true;
-    });
+    buttons.forEach( (button) => { button.disabled = true; });
     menu.style.display='none';
     button.form.appendChild(menu);
     button.form.submit();
@@ -26,50 +24,51 @@ menu.addEventListener('change', toggleDisplays);
 
 function toggleDisplays () {
   if(menu.value === '1'){
-    dataMenuOne.forEach( (elem) => {
-      elem.style.display='inline';
-    });
-    dataMenuTwo.forEach( (elem) => {
-      elem.style.display='none';
-    });
-    dataMenuThree.forEach( (elem) => {
-      elem.style.display='none';
-    });
+    dataMenuOne.forEach( (elem) => { elem.style.display='inline'; });
+    dataMenuTwo.forEach( (elem) => { elem.style.display='none'; });
+    dataMenuThree.forEach( (elem) => { elem.style.display='none'; });
   }
   else if(menu.value === '2'){
-    dataMenuOne.forEach( (elem) => {
-      elem.style.display='none';
-    });
-    dataMenuTwo.forEach( (elem) => {
-      elem.style.display='inline';
-    });
-    dataMenuThree.forEach( (elem) => {
-      elem.style.display='none';
-    });
+    dataMenuOne.forEach( (elem) => { elem.style.display='none'; });
+    dataMenuTwo.forEach( (elem) => { elem.style.display='inline'; });
+    dataMenuThree.forEach( (elem) => { elem.style.display='none'; });
   }
   else if(menu.value === '3'){
-    dataMenuOne.forEach( (elem) => {
-      elem.style.display='none';
-    });
-    dataMenuTwo.forEach( (elem) => {
-      elem.style.display='none';
-    });
-    dataMenuThree.forEach( (elem) => {
-      elem.style.display='inline';
-    });    
+    dataMenuOne.forEach( (elem) => { elem.style.display='none'; });
+    dataMenuTwo.forEach( (elem) => { elem.style.display='none'; });
+    dataMenuThree.forEach( (elem) => { elem.style.display='inline'; });    
   }
   else {
-    dataMenuOne.forEach( (elem) => {
-      elem.style.display='none';
-    });
-    dataMenuTwo.forEach( (elem) => {
-      elem.style.display='none';
-    });
-    dataMenuThree.forEach( (elem) => {
-      elem.style.display='none';
-    });    
+    dataMenuOne.forEach( (elem) => { elem.style.display='none'; });
+    dataMenuTwo.forEach( (elem) => { elem.style.display='none'; });
+    dataMenuThree.forEach( (elem) => { elem.style.display='none'; });    
   }
 };
+
+const body_shape_elem = document.querySelector('#body-shape');
+body_shape_elem.addEventListener('change', () => {
+  if(body_shape_elem.value === 'a'){
+    add_input_row('hull nose coordinates: ', 'hull-nose-coords');
+    add_input_row('next hull control point coordinates: ', 'next-hull-ctrl-pt-coords');
+    add_input_row('# of segements between control points: ', 'segments-between-ctrl-pts');
+  }
+  else if(body_shape_elem.value === 'f'){
+
+  }
+});
+
+function add_input_row (text_desc, input_name) {
+  const tr = document.createElement('tr');
+  const td_a = document.createElement('td');
+  td_a.innerText = text_desc;
+  const td_b = document.createElement('td');
+  const input = document.createElement('input');
+  input.name = input_name;
+  td_b.appendChild(input);
+  tr.appendChild(td_a);
+  tr.appendChild(td_b);
+  body_shape_elem.form.querySelector('table').appendChild(tr);
+}
 
 // Browser back button
 window.addEventListener('pageshow', toggleDisplays);
